953c5942d9a950a15df3b60f52441cf29afc8410
LucasLima <lugablima@gmail.com>
2022-07-20 15:23:30 -0300

    chore: add initial project configuration

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./setup.js";
+import express, { json } from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors(), json());
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
