ebfa591b864359b8d9e85af5b1aaa5af219b1302
LucasLima <lugablima@gmail.com>
2022-07-25 11:57:38 -0300

    chore: add POST route '/rentals/:id/return'

--- src/controllers/rentalsController.js
+++ src/controllers/rentalsController.js
@@ -20,7 +20,8 @@ export async function getRentals(req, res) {
       JOIN games
       ON rentals."gameId" = games.id
       JOIN categories
-      ON games."categoryId" = categories.id`
+      ON games."categoryId" = categories.id
+      ORDER BY rentals.id ASC`
       );
 
       rentalsInfos = rows;
@@ -34,7 +35,8 @@ export async function getRentals(req, res) {
       ON rentals."gameId" = games.id
       JOIN categories
       ON games."categoryId" = categories.id
-      WHERE rentals."gameId" = $1`,
+      WHERE rentals."gameId" = $1
+      ORDER BY rentals.id ASC`,
         [gameId]
       );
 
@@ -49,7 +51,8 @@ export async function getRentals(req, res) {
       ON rentals."gameId" = games.id
       JOIN categories
       ON games."categoryId" = categories.id
-      WHERE rentals."customerId" = $1`,
+      WHERE rentals."customerId" = $1
+      ORDER BY rentals.id ASC`,
         [customerId]
       );
 
@@ -64,7 +67,8 @@ export async function getRentals(req, res) {
       ON rentals."gameId" = games.id
       JOIN categories
       ON games."categoryId" = categories.id
-      WHERE rentals."gameId" = $1 AND rentals."customerId" = $2`,
+      WHERE rentals."gameId" = $1 AND rentals."customerId" = $2
+      ORDER BY rentals.id ASC`,
         [gameId, customerId]
       );
 
@@ -82,6 +86,8 @@ export async function getRentals(req, res) {
           categoryName: rental.categoryName,
         },
       };
+
+      if (rentalFinal.returnDate) rentalFinal.returnDate = dayjs(rentalFinal.returnDate).format("YYYY-MM-DD");
 
       delete rentalFinal.customerName;
       delete rentalFinal.gameName;
@@ -118,6 +124,75 @@ export async function createRental(req, res) {
     res.sendStatus(201);
   } catch (err) {
     console.log("Error while creating a new rental", err.message);
+    res.sendStatus(500);
+  }
+}
+
+export async function finalizeRental(req, res) {
+  const { id } = req.params;
+
+  try {
+    let {
+      rows: [rental],
+    } = await connection.query(
+      `SELECT *, to_char("rentDate", 'YYYY-MM-DD') as "rentDate" FROM rentals
+    WHERE id = $1`,
+      [id]
+    );
+
+    if (!rental) return res.sendStatus(404);
+
+    if (rental.returnDate) return res.sendStatus(400);
+
+    const currentDate = dayjs(dayjs().format("YYYY-MM-DD"));
+    const expectedReturnDate = dayjs(rental.rentDate).add(rental.daysRented, "day");
+    const dateDifferenceInDays = currentDate.diff(expectedReturnDate, "day");
+
+    if (dateDifferenceInDays >= 0) {
+      const {
+        rows: [game],
+      } = await connection.query(`SELECT * FROM games WHERE id = $1`, [rental.gameId]);
+
+      rental.delayFee = dateDifferenceInDays * game.pricePerDay;
+    } else rental.delayFee = 0;
+
+    rental = { ...rental, returnDate: dayjs().format("YYYY-MM-DD") };
+
+    await connection.query(
+      `UPDATE rentals
+     SET "customerId" = $1, "gameId" = $2, "rentDate" = $3, "daysRented" = $4, "returnDate" = $5, "originalPrice" = $6, "delayFee" = $7
+     WHERE id = $8`,
+      [rental.customerId, rental.gameId, rental.rentDate, rental.daysRented, rental.returnDate, rental.originalPrice, rental.delayFee, id]
+    );
+
+    res.status(200).send(rental);
+  } catch (err) {
+    console.log("Error while finalizing a rental", err.message);
+    res.sendStatus(500);
+  }
+}
+
+export async function deleteRental(req, res) {
+  const { id } = req.params;
+
+  try {
+    const {
+      rows: [rental],
+    } = await connection.query(
+      `SELECT * FROM rentals
+    WHERE id = $1`,
+      [id]
+    );
+
+    if (!rental) return res.sendStatus(404);
+
+    if (!rental.returnDate) return res.sendStatus(400);
+
+    await connection.query(`DELETE FROM rentals WHERE id = $1`, [id]);
+
+    res.sendStatus(200);
+  } catch (err) {
+    console.log("Error while deleting a rental", err.message);
     res.sendStatus(500);
   }
 }
--- src/routes/rentalsRouter.js
+++ src/routes/rentalsRouter.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { getRentals, createRental } from "../controllers/rentalsController.js";
+import { getRentals, createRental, finalizeRental, deleteRental } from "../controllers/rentalsController.js";
 import validateRental from "../middlewares/rentalsMiddlewares.js";
 
 const router = Router();
 
 router.get("/rentals", getRentals);
 router.post("/rentals", validateRental, createRental);
+router.post("/rentals/:id/return", finalizeRental);
+router.delete("/rentals/:id", deleteRental);
 
 export default router;
